03d0a5f87d29deaf1c83a120990a78ad882715e2
Simon Lin <simonlin.rqmmw@slmail.me>
2026-06-08 21:00:57 +1000

    c

--- crates/polars-lazy/src/frame/mod.rs
+++ crates/polars-lazy/src/frame/mod.rs
@@ -627,7 +627,7 @@ impl LazyFrame {
                 DslPlan::Scan {
                     unified_scan_args, ..
                 } => unified_scan_args.rechunk,
-                DslPlan::SinkMultiple { inputs } => inputs.iter().any(is_scan_with_rechunk),
+                DslPlan::SinkMultiple { inputs } => inputs.iter().all(is_scan_with_rechunk),
                 DslPlan::Sink { input, .. } => is_scan_with_rechunk(input),
                 _ => false,
             }
