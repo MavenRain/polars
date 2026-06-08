d4eacf9a5ea0ca9682a41eade4056914c4c1f39c
Simon Lin <simonlin.rqmmw@slmail.me>
2026-06-08 19:36:52 +1000

    c

--- py-polars/src/polars/dataframe/frame.py
+++ py-polars/src/polars/dataframe/frame.py
@@ -8369,8 +8369,8 @@ class DataFrame:
         ╞══════╪══════╪═════╪═══════╡
         │ 1    ┆ 6.0  ┆ a   ┆ x     │
         │ 2    ┆ 7.0  ┆ b   ┆ y     │
-        │ 3    ┆ 8.0  ┆ c   ┆ null  │
         │ null ┆ null ┆ d   ┆ z     │
+        │ 3    ┆ 8.0  ┆ c   ┆ null  │
         └──────┴──────┴─────┴───────┘
 
         >>> df.join(other_df, on="ham", how="left")
